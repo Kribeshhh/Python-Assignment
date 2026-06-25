352d594dbe31a20e97292a67877eaf32c4f7dc3c
Kribesh Thapa <kribeshthapa@Kribeshs-MacBook-Pro.local>
2026-06-25 17:56:13 +0545

    Remove unwanted files

--- 5.py
+++
@@ -1,8 +0,0 @@
-def convert_to_uppercase(strings):
-    return list(map(lambda x: x.upper(), strings))
-
-words = input("Enter words separated by spaces: ").split()
-
-result = convert_to_uppercase(words)
-
-print("Uppercase Words:", result)
--- 6.py
+++
@@ -1,8 +0,0 @@
-def filter_even_length_words(words):
-    return list(filter(lambda x: len(x) % 2 == 0, words))
-
-words = input("Enter words separated by spaces: ").split()
-
-result = filter_even_length_words(words)
-
-print("Even Length Words:", result)
